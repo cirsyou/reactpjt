a0a15bc1c97933ab95204b0f667db30155466117
cris <crisliu@aliyun.com>
2020-02-20 10:39:48 +0800

    调整项目结构

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -295,6 +295,7 @@ module.exports = function(webpackEnv) {
           'react-dom$': 'react-dom/profiling',
           'scheduler/tracing': 'scheduler/tracing-profiling',
         }),
+        '@': path.join(__dirname, '../src/'),
         ...(modules.webpackAliases || {}),
       },
       plugins: [
