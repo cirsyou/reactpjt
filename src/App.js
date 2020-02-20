f91a99e8383ae12a51e255c1fe93cbc12a9a88cd
cris <crisliu@aliyun.com>
2020-02-20 10:50:06 +0800

    add project menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import '@/assets/style/App.css';
+// 局部加载样式表
+import LayoutCss from '@/assets/style/layout.module.scss';
+// 引入路由文件
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends React.Component {
-  render () {
+  render() {
     return (
-      <div className="App">
-        react app
+      <Router>
+        <div className={LayoutCss.wrapper}>
+
+          react app
       </div>
+      </Router>
     );
   }
 }
