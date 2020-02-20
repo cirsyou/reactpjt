2f8fc295fd447f3d0c2c7e9391d774e9ba6fe16a
cris <crisliu@aliyun.com>
2020-02-20 11:32:04 +0800

    add build files

---
+++ build/precache-manifest.9787cba6ea0e4d3b00d80a7464bbc0d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "208b849a14ca861cdb6ac4f74a0b0963",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf6e1c43378ca0e05c5e",
+    "url": "/static/css/main.047b5e0b.chunk.css"
+  },
+  {
+    "revision": "7f16a59b5f1b814f8609",
+    "url": "/static/js/2.3a35678b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.3a35678b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf6e1c43378ca0e05c5e",
+    "url": "/static/js/main.8c61fe2e.chunk.js"
+  },
+  {
+    "revision": "010615119612d66e518a",
+    "url": "/static/js/runtime-main.69c07191.js"
+  }
+]);
